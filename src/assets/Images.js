9829b653c818b48f80e1e9cbc566c0fe04623a9a
shavuparmar <parmarshavu009@gmail.com>
2025-08-17 11:47:02 +0530

    changes image folder

--- src/assets/Images.js
+++ src/assets/Images.js
@@ -1,41 +1,48 @@
-import Code from "./code.png"
-import Coderimage from "./CoderImage.jpg"
-import ColorGuide from "./color-guide.png"
-import Designer from "./designer.png"
-import Designlaptop from "./DesignLaptop.jpg"
-import Developerimg from "./developer.png"
-import GraphicsDesign from "./Graphics-Design.jpg"
-import Logo from "./MainLogo.png"
-import EditingImage from "./photo-editing.png"
-import PhotoshopEditing from "./Photoshop-Editing.jpg"
-import Posterimg from "./Poster.png"
-import Uidesign from "./ui-design.png"
-import UiUximg from "./UIUx-Representing.jpg"
-import Backgroundimage from "./bgimage.jpg"
-import Aboutimage from "./AboutImage.jpg"
-import Contactimg from "./Contact.jpg"
+import Code from "./code.png";
+import Coderimage from "./CoderImage.jpg";
+import ColorGuide from "./color-guide.png";
+import Designer from "./designer.png";
+import Designlaptop from "./DesignLaptop.jpg";
+import Developerimg from "./developer.png";
+import GraphicsDesign from "./Graphics-Design.jpg";
+import Logo from "./MainLogo.png";
+import EditingImage from "./photo-editing.png";
+import PhotoshopEditing from "./Photoshop-Editing.jpg";
+import Posterimg from "./Poster.png";
+import Uidesign from "./ui-design.png";
+import UiUximg from "./UIUx-Representing.jpg";
+import Backgroundimage from "./bgimage.jpg";
+import Aboutimage from "./AboutImage.jpg";
+import Contactimg from "./Contact.jpg";
 
+// Brands Logos & Images imported here
 
-// Brands Logos & Images imported here 
+import Devhubs from "./Devhubs.png";
+import ParmarMusic from "./ParmarMusic.png";
+import JalaXerox from "./JalaXerox.png";
+import Parmarin from "./infotech.png";
+import Zyntronix from "./Zyntronix.png";
 
-import Devhubs from "./Brands/Devhubs.png"
-import ParmarMusic from "./Brands/ParmarMusic.png"
-import JalaXerox from "./Brands/JalaXerox.png"
-import Parmarin from "./Brands/infotech.png"
-import Zyntronix from "./Brands/Zyntronix.png"
-
-
-
-
-
-
-
-
-
-
-export { Code , Coderimage, ColorGuide , Designer , Designlaptop , Developerimg , GraphicsDesign, Logo , EditingImage , PhotoshopEditing , Posterimg , UiUximg , Uidesign , Backgroundimage , Contactimg , Aboutimage 
-, Devhubs , ParmarMusic , JalaXerox  , Zyntronix , Parmarin
-
-
-
-}
+export {
+  Code,
+  Coderimage,
+  ColorGuide,
+  Designer,
+  Designlaptop,
+  Developerimg,
+  GraphicsDesign,
+  Logo,
+  EditingImage,
+  PhotoshopEditing,
+  Posterimg,
+  UiUximg,
+  Uidesign,
+  Backgroundimage,
+  Contactimg,
+  Aboutimage,
+  Devhubs,
+  ParmarMusic,
+  JalaXerox,
+  Zyntronix,
+  Parmarin,
+};
